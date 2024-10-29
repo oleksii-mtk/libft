83de49ae9adbd7b2e975e1d4c238b0f654b50186
Oleksii Matyko <omatyko@c2r4p1.hive.fi>
2024-10-29 15:54:40 +0200

    ft_isascii added

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omatyko <omatyko@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 15:24:45 by omatyko           #+#    #+#             */
+/*   Updated: 2024/10/29 15:52:41 by omatyko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int     ft_isascii(int c);
+
+
+#endif
