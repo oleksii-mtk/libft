3cdd9d57716bd5cbc09c81c99537db558a39372d
Alex Matyko <alexa.ua@gmail.com>
2024-10-30 18:57:56 +0200

    New files added

--- libft.h
+++ libft.h
@@ -6,17 +6,17 @@
 /*   By: omatyko <omatyko@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:24:45 by omatyko           #+#    #+#             */
-/*   Updated: 2024/10/29 15:52:41 by omatyko          ###   ########.fr       */
+/*   Updated: 2024/10/30 16:20:09 by omatyko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 #define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int     ft_isascii(int c);
-
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
 
 #endif
