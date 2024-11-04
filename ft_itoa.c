4bb0e49621b705cc2a8bc8b4cb6aa3e401d28df1
Alex Matyko <alexa.ua@gmail.com>
2024-11-04 10:09:00 +0200

    ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omatyko <omatyko@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 10:02:18 by omatyko           #+#    #+#             */
+/*   Updated: 2024/11/04 10:07:49 by omatyko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	get_num_length(int n)
+{
+	int	length;
+
+	length = 0;
+	if (n <= 0)
+		length = 1;
+	while (n != 0)
+	{
+		n /= 10;
+		length++;
+	}
+	return (length);
+}
+
+char	*ft_itoa(int n)
+{
+	int		length;
+	char	*result;
+
+	length = get_num_length(n);
+	result = (char *)malloc(sizeof(char) * (length + 1));
+	if (!result)
+		return (NULL);
+	result[length] = '\0';
+	if (n < 0)
+	{
+		result[0] = '-';
+		n = -n; //
+	}
+	else if (n == 0)
+	{
+		result[0] = '0';
+		result[0] = '\0';
+		return (result);
+	}
+	while (n > 0)
+	{
+		result[--length] = (n % 10) + '0';
+		n /= 10;
+	}
+	return (result);
+}
