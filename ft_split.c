8a8f064450fdcb3ea1a26617744625d0ce2dab5f
Alex Matyko <alexa.ua@gmail.com>
2024-11-04 09:56:15 +0200

    Adding ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,92 +6,92 @@
 /*   By: omatyko <omatyko@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 09:18:43 by omatyko           #+#    #+#             */
-/*   Updated: 2024/11/04 09:35:00 by omatyko          ###   ########.fr       */
+/*   Updated: 2024/11/04 09:48:27 by omatyko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t count_words(const char *s, char c)
+static size_t	count_words(const char *s, char c)
 {
-    size_t count = 0;
-    int in_word = 0;
+	size_t	count;
+	int		in_word;
 
-    while (*s)
-    {
-        if (*s != c && !in_word)
-        {
-            in_word = 1;
-            count++;
-        }
-        else if (*s == c)
-        {
-            in_word = 0;
-        }
-        s++;
-    }
-    return (count);
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && !in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+		{
+			in_word = 0;
+		}
+		s++;
+	}
+	return (count);
 }
 
-static char *copy_word(const char *start, size_t length)
+static char	*copy_word(const char *start, size_t length)
 {
-    char *word;
-    size_t i;
+	char	*word;
+	size_t	i;
 
-    word = (char *)malloc(sizeof(char) * (length + 1));
-    if (!word)
-        return (NULL);
-
-    i = 0;
-    while (i < length)
-    {
-        word[i] = start[i];
-        i++;
-    }
-    word[i] = '\0';
-
-    return (word);
+	word = (char *)malloc(sizeof(char) * (length + 1));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < length)
+	{
+		word[i] = start[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char **result;
-    size_t word_count;
-    size_t i = 0;
-    size_t len = 0;
-    const char *word_start;
+	char **result;
+	size_t word_count;
+	size_t i = 0;
+	size_t len = 0;
+	const char *word_start;
 
-    if (!s)
-        return (NULL);
+	if (!s)
+		return (NULL);
 
-    word_count = count_words(s, c);
-    result = (char **)malloc(sizeof(char *) * (word_count + 1));
-    if (!result)
-        return (NULL);
+	word_count = count_words(s, c);
+	result = (char **)malloc(sizeof(char *) * (word_count + 1));
+	if (!result)
+		return (NULL);
 
-    while (*s)
-    {
-        if (*s != c)
-        {
-            len = 0;
-            word_start = s;
-            while (*s && *s != c)
-            {
-                len++;
-                s++;
-            }
-            result[i++] = copy_word(word_start, len);
-            if (!result[i - 1])
-            {
-                while (i > 0)
-                    free(result[--i]);
-                free(result);
-                return (NULL);
-            }
-        }
-        else
-            s++;
-    }
-    result[i] = NULL;
-    return (result);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			word_start = s;
+			while (*s && *s != c)
+			{
+				len++;
+				s++;
+			}
+			result[i++] = copy_word(word_start, len);
+			if (!result[i - 1])
+			{
+				while (i > 0)
+					free(result[--i]);
+				free(result);
+				return (NULL);
+			}
+		}
+		else
+			s++;
+	}
+	result[i] = NULL;
+	return (result);
 }
